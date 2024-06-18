25984ae155917c0cbc19c1eec5a81d5968429661
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-18 14:33:16 +0530

    add slick-slider and hover,transition effect on img

--- src/components/PopularSlider.jsx
+++ src/components/PopularSlider.jsx
@@ -1,4 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import Slider from "react-slick";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const PopularSlider = () => {
     const [data, setData] = useState([])
@@ -15,22 +21,45 @@ const PopularSlider = () => {
   
       fetchData();
     }, [])
+
+    
+  var settings = {
+    // dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
+  };
+
   return (
     <>
-    
-    {data.map((d) => {
-        return (
-   <div>
-   <img src={d.strMealThumb} alt="" style={{ width:'18rem', height:'17rem' }} />
-   
-   </div>
+      <div style={{
+        height:'56vh',
+        width:'90%',
+        margin:'auto',
+        // backgroundColor:'yellow'
+      }} >
 
-         
-           
-         
-        )
-      })}
-</>
+        <Slider {...settings}
+      style={{
+       margin:'1rem'
+      }} 
+        >
+          {data.map((d) => {
+            return (
+
+              <div className='slider'>
+                <img src={d.strMealThumb} alt="" style={{ width:'18rem', height:'17rem' }} />
+              </div>
+              
+            )
+          })}
+
+        </Slider>
+      </div>
+    </>
     )
 }
 export default PopularSlider
