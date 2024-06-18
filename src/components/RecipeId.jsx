e7faeb71d56166720dbffcebfc6ec784d54f8888
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-18 15:07:37 +0530

    fetch data by id

--- src/components/RecipeId.jsx
+++ src/components/RecipeId.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
+import TrendingSlider from './TrendingSlider'
+import { useParams } from 'react-router-dom'
+
 
 const RecipeId = () => {
+  const {idMeal} = useParams()
+  // console.log(useParams())
+
+  const [data, setData] = useState([])
+  
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const api = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+      const data = await api.json();
+
+      // console.log(data.meals);
+      setData(data.meals[0])
+      console.log(data)
+    }
+
+    fetchData();
+  }, [idMeal])
+
   return (
-    <div>RecipeId</div>
+    <>
+    <Navbar />
+    <TrendingSlider />
+    </>
   )
 }
 
