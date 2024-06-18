9548abf65faaf0fc9f0d3dd7d6d4354275e2c03f
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-18 16:48:09 +0530

    ingredient and instruction button active

--- src/components/RecipeId.jsx
+++ src/components/RecipeId.jsx
@@ -9,7 +9,7 @@ const RecipeId = () => {
   // console.log(useParams())
 
   const [data, setData] = useState([])
-  
+  const [active, setActive] = useState('ingredient')
 
   useEffect(() => {
 
@@ -48,9 +48,27 @@ const RecipeId = () => {
         <img src={data.strMealThumb} alt="" style={{width:'18rem'}} />
      </div>
      <div className="content" style={{width:'60%'}}>
-     <button className="btn">Ingredient</button>
-     <button className="btn" >Instruction</button>
+     <button className="btn" onClick={()=>setActive('ingredient')}>Ingredient</button>
+     <button className="btn" onClick={()=>setActive('instruction')}>Instruction</button>
 
+
+     {
+
+      active === 'ingredient' ? (
+        <div>
+          <div style={{fontSize:'1.5rem', fontWeight:'bold'}}>{data.strIngredient1} - {data.strMeasure1}</div>
+          <div style={{fontSize:'1.5rem', fontWeight:'bold'}}>{data.strIngredient2} - {data.strMeasure2}</div>
+          <div style={{fontSize:'1.5rem', fontWeight:'bold'}}>{data.strIngredient3} - {data.strMeasure3}</div>
+          <div style={{fontSize:'1.5rem', fontWeight:'bold'}}>{data.strIngredient4} - {data.strMeasure4}</div>
+          <div style={{fontSize:'1.5rem', fontWeight:'bold'}}>{data.strIngredient5} - {data.strMeasure5}</div>
+          <div style={{fontSize:'1.5rem', fontWeight:'bold'}}>{data.strIngredient6} - {data.strMeasure6}</div>
+        </div>
+    ) : (
+      <p>{data.strInstructions} </p>
+    )
+    }
+
+    
      </div>
       </div>
 
