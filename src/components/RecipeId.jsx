8c3544f2e51475e5a855a2c646742c899cc1e201
hemantagarwal707 <hemantagarwal707@gmail.com>
2024-06-18 16:10:03 +0530

    design detail page

--- src/components/RecipeId.jsx
+++ src/components/RecipeId.jsx
@@ -28,7 +28,41 @@ const RecipeId = () => {
   return (
     <>
     <Navbar />
+    <div 
+    style={{
+        width:'90%' ,
+        margin:'auto',
+        textAlign:'center'
+    }}
+    >
+      <h1>{data.strMeal}</h1>
+      <div 
+       style={{
+       display:'flex'
+    }}
+      >
+        
+     <div className="img"
+      style={{width:'30%', marginTop:'2rem'}}
+      >
+        <img src={data.strMealThumb} alt="" style={{width:'18rem'}} />
+     </div>
+     <div className="content" style={{width:'60%'}}>
+     <button className="btn">Ingredient</button>
+     <button className="btn" >Instruction</button>
+
+     </div>
+      </div>
+
+
+
+
+    </div>
+    <div style={{marginTop:'1rem'}}>
+
     <TrendingSlider />
+    </div>
+    
     </>
   )
 }
